4cac41447c890291db44712e7b535f3dcb0e96a3
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-01-02 14:03:00 +0530

    exercise_tracker:done

--- src/api/exercise_session.py
+++ src/api/exercise_session.py
@@ -61,3 +61,73 @@ class ExerciseTracker:
         else:
             return "Start position", (255, 255, 255)
         
+    def process_frame(self, frame, frame_timestamp_ms):
+        frame = cv2.flip(frame, 1)
+        
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        mp_image = mp.Image(image_format = mp.ImageFormat.SRGB, data=image)
+        
+        results = pose_landmarker.detect_for_video(mp_image, frame_timestamp_ms)
+        image.flags.writeable = Trueimage = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        metrics = {
+            "angle": 0,
+            "reps": self.rep_count,
+            "max_angle": self.max_angle,
+            "feedback": "Position yourself in frame",
+            "pse_detectd": False,
+        }
+        
+        try:
+            if results.pose_landmarks:
+                landmarks = results.pose_landmarks[0]
+                metrics["pose_detected"] = True
+                shoulder = [landmarks[12].X, landmarks[12].Y]
+                elbow = [landmarks[14].X, landmarks[14].Y]
+                
+                angle = self.calculate_angle(elbow, shoulder, [shoulder[0], shoulder[1] - 0.1])
+                metrics["angle"] = round(angle, 1)
+                
+                if angle < 30:
+                    self.stage = "down"
+                if angle > 140 and self.stage == "down":
+                    self.stage = "up"
+                    self.re_count += 1
+                    metrics["reps"] = self.rep_count
+                    
+                feedback_text, color = self.get_feedback(angle)
+                metrics["feedback"] = feedback_text
+                
+                self.angle_history.append(angle)
+                self.draw_landmarks(image, landmarks)
+                
+                shoulder_px = tuple(np.multiply(shoulder, [image.shape[1], image.shape[0]]).astype(int))
+                cv2.putText(image, f"{int(angle)}degree", shoulder_px,
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                
+                
+                cv2.rectangle(image, (0, 0), (400, 150), (0,0,0), -1)
+                cv2.putText(image, 'NEUROPATH AI', (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 155, 255), 2)
+                
+                cv2.putText(image, f'Reps: {self.rep_count}', (10, 65),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+                
+                cv2.putText(image, f'Current: {int(angle)}degree', (10, 95),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 0), 2)
+                
+                cv2.putText(image, f'Best: {int(self.max_angle)}degree', (10, 125),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
+                
+                
+                
+                cv2.putText(image, feedback_text, (10, image.shape[0]-20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                
+                
+        except Exception as e:
+            cv2.pustText(image, 'Position yourself in frame', (50, 240),
+                         cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+            
+        return image, metrics
