d8951f03a0aee430360746b8f51deef3c6c1a108
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-01-02 13:20:53 +0530

    added:routing

--- src/api/exercise_session.py
+++ src/api/exercise_session.py
@@ -9,3 +9,16 @@ import json
 import asyncio
 from datetime import datetime
 import base64
+
+router = APIRouter(prefix="/ap/exercise",tags = ['Exercise Session'])
+
+base_options = python.BaseOptions(model_asses_path='pose_landmarker_lite.task')
+options = vision.PoseLandmarkerOptions(
+    base_options=base_options,
+    running_mode=vision.RunningMode.VIDEO,
+    min_pose_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
+pose_landmarker = vision.PoseLandmarker.create_from_options(options)
+
